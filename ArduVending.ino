d5edf55328afde01122ac8d694503d41560b20ec
lmalmoreno <lmalmoreno@gmail.com>
2014-09-02 10:45:07 -0300

    Leitura Serial
    Adição da leitura da serial e armazenamento em uma variável.

--- ArduVending.ino
+++ ArduVending.ino
@@ -1,12 +1,34 @@
 //IDE 1.05
 //TCC ENGENHENHARIA ELÉTRICA 2014
 
+int serIn; //Variável para armazenar a leitura da serial
+
 void setup(){
+  Serial.begin(9600); //Inicialização da porta serial
   
 }
 
 void loop(){
   
+  if(Serial.available()) //Se tiver dados na porta serial
+    { 
+      while (Serial.available()>0) //Enquanto tiver dados na serial 
+        {
+          serIn = Serial.read(); //Armazena os dados lidos na variável serIn
+          Serial.write(serIn);  //Escreve os dados armazenados na serial (Apenas Debug)
+        }
+      Serial.println(); //Imprime de acordo com ASCII
+    }
+
+delay(1000);
+
+// Inicio das comparações
+
+  if (serIn == 01)
+    {
+       // execução
+    }
+
 }
 
 
