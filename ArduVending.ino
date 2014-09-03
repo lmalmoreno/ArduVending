41429a34dc955ccb2d288c000503da3458f111db
lmalmoreno <lmalmoreno@gmail.com>
2014-09-03 11:22:15 -0300

    Pareamento
    Adicionado bloco de pareamento

--- ArduVending.ino
+++ ArduVending.ino
@@ -2,6 +2,9 @@
 //TCC ENGENHENHARIA ELÉTRICA 2014
 
 int serIn; //Variável para armazenar a leitura da serial
+int KEY = 123456;
+int RKEY = 0;
+
 
 void setup(){
   Serial.begin(9600); //Inicialização da porta serial
@@ -9,6 +12,34 @@ void setup(){
 }
 
 void loop(){
+  
+  //BLOCO DE PAREAEMENTO (COM ERROS)
+  
+  while(RKEY != KEY) // Enquanto a chave informada for diferente da chave na memória
+  {
+  Serial.println("SP"); //Escreve SP na serial informando que está pronto para parear
+  delay (100);
+   if(Serial.available()) //Se tiver dados na porta serial
+    { 
+      while (Serial.available()>0) //Enquanto tiver dados na serial 
+        {
+          RKEY = Serial.read(); //Armazena os dados lidos na variável RKEY
+          Serial.write(RKEY);  //Escreve os dados armazenados na serial (Apenas Debug)
+        }
+      Serial.println(); //Imprime de acordo com ASCII
+    }
+    
+     if (RKEY == KEY){
+       Serial.println("IP"); //Escreve IP na serial informando que foi pareado com sucesso
+    }
+    else{
+       Serial.println("EP"); //Escreve EP na serial informando que não foi pareado
+       delay (1000);
+    }
+}
+  
+  
+  
   
   if(Serial.available()) //Se tiver dados na porta serial
     { 
